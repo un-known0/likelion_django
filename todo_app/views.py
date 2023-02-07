0b12f4e4c282f7c1239990f2747f55bc57b724d5
root <root@DESKTOP-AI8E9HH.localdomain>
2023-02-07 16:40:59 +0900

    할 일 목록 페이지 만들기

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Todo
 
 # Create your views here.
+def index(request):
+    # todos = Todo.objects.all().filter(todo__contains='첫번째')
+    todos = Todo.objects.all().order_by('-pk')
+    return render(request,'todo_app/index.html', {'todos':todos,})
